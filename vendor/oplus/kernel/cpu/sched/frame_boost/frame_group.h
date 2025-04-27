89a9af543a22c7503019e128600dbd75aa63d118
pswbuild <pswbuild@oppo.com>
2025-04-27 22:46:35 +0800

    Synchronize code for OPPO PKV110_15.0.0.702(CN01) Based on MTK release TAG: alps/vendor/mediatek/kernel_modules/connectivity/bt/mt66xx:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/bt/mt76xx/sdio:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/common:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/connfem:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/conninfra:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/fmradio:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/gps:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/wlan/adaptor:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/wlan/core/gen2:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/wlan/core/gen3:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/wlan/core/gen4-mt7663:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/wlan/core/gen4-mt7668:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/connectivity/wlan/core/gen4-mt79xx:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/gpu:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/hbt_driver:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/hbt_driver_cus:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/met_drv_v2:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/met_drv_v3:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 alps/vendor/mediatek/kernel_modules/udc:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 kernel/build:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 neptune/bt_driver/linux_v2:t-alps-release-t0.mp1.tc16sp-pr5-V1.62 neptune/wlan_driver/gen4m:t-alps-release-t0.mp1.tc16sp-pr5-V1.62

---
+++ vendor/oplus/kernel/cpu/sched/frame_boost/frame_group.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2022 Oplus. All rights reserved.
+ */
+#ifdef CONFIG_OPLUS_FEATURE_MULTI_FBG
+#include "./multi_rtg/frame_group.h"
+#else
+#include "./frame_boost_V1/frame_group.h"
+#endif
+
